733544e6d6b549293a43586ec8bda6dc833e686d
deanwbusch-prog <deanwbusch@gmail.com>
2025-09-16 16:16:16 -0500

    Delete serverless-todo-app/frontend/is directory

--- serverless-todo-app/frontend/is/config.template.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  apiBaseUrl: "https://xxxxxx.execute-api.us-west-1.amazonaws.com", // <- OUTPUT: HttpApiUrl (no trailing slash)
-  cognito: {
-    region: "us-west-1",
-    userPoolId: "us-west-1_XXXXXXXXX",        // <- OUTPUT: UserPoolId
-    userPoolClientId: "XXXXXXXXXXXXXXXXXXXX", // <- OUTPUT: UserPoolClientId
-  }
-};
