e601f2015d65666cf253ceb69a1ca2747e98d46a
deanwbusch-prog <deanwbusch@gmail.com>
2025-09-16 16:29:02 -0500

    Create outputs.tf

---
+++ iac-terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "ec2_public_ip" {
+  value       = module.ec2.public_ip
+  description = "Public IP of the EC2 instance"
+}
