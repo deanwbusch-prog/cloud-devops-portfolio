66c72997d442295f1eb91fd947d7c8b624e32617
deanwbusch-prog <deanwbusch@gmail.com>
2025-09-16 16:26:23 -0500

    Update outputs.tf

--- iac-terraform/bootstrap/outputs.tf
+++ iac-terraform/bootstrap/outputs.tf
@@ -1,16 +1,7 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
+output "state_bucket" {
+  value = aws_s3_bucket.state.bucket
 }
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
-
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
-
-output "ec2_public_ip" {
-  value       = module.ec2.public_ip
-  description = "Public IP of the EC2 instance"
+output "lock_table" {
+  value = aws_dynamodb_table.lock.name
 }
