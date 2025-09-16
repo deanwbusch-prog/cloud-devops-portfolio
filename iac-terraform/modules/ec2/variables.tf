e0a0207e8b6a85a8c3e59f8f7dee6067231fdc13
deanwbusch-prog <deanwbusch@gmail.com>
2025-09-16 16:32:51 -0500

    Create variables.tf

---
+++ iac-terraform/modules/ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "project"         { type = string }
+variable "subnet_id"       { type = string }
+variable "vpc_id"          { type = string }
+variable "allowed_ssh_cidr"{ type = string }
+variable "key_name"        { type = string }
+variable "instance_type"   { type = string }
