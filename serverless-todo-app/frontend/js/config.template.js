fdf12a66ff5940c283efaa41f5060986655148de
deanwbusch-prog <deanwbusch@gmail.com>
2025-09-18 13:08:29 -0500

    Update config.template.js

--- serverless-todo-app/frontend/js/config.template.js
+++ serverless-todo-app/frontend/js/config.template.js
@@ -1,8 +1,8 @@
 window.APP_CONFIG = {
-  apiBaseUrl: "https://xxxxxx.execute-api.us-west-1.amazonaws.com", // <- OUTPUT: HttpApiUrl (no trailing slash)
+  apiBaseUrl: "https://740ygy9xtk.execute-api.us-east-2.amazonaws.com", // <- OUTPUT: HttpApiUrl (no trailing slash)
   cognito: {
-    region: "us-west-1",
-    userPoolId: "us-west-1_XXXXXXXXX",        // <- OUTPUT: UserPoolId
-    userPoolClientId: "XXXXXXXXXXXXXXXXXXXX", // <- OUTPUT: UserPoolClientId
+    region: "us-east-2",
+    userPoolId: "us-east-2_6Y9E3tgYX",        // <- OUTPUT: UserPoolId
+    userPoolClientId: "4bk1bttj1jb0eepn9smouo08am", // <- OUTPUT: UserPoolClientId
   }
 };
