1527d6771ca8949bf7bc800f97de234cd137bc97
deanwbusch-prog <deanwbusch@gmail.com>
2025-12-24 16:07:09 -0600

    Update outputs.tf

--- disaster-recovery-plan/infra/terraform/outputs.tf
+++ disaster-recovery-plan/infra/terraform/outputs.tf
@@ -1,1 +1,14 @@
+output "backup_vault_name" {
+  value       = aws_backup_vault.dr_vault.name
+  description = "Name of the backup vault"
+}
 
+output "backup_plan_name" {
+  value       = aws_backup_plan.dr_plan.name
+  description = "Name of the backup plan"
+}
+
+output "aws_region" {
+  value       = var.aws_region
+  description = "AWS region where resources are deployed"
+}
